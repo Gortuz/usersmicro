8d01d27d4a6cc5240e1413f6b08b504c454bf538
Alan Sandoval <alansandovalproject@gmail.com>
2026-05-09 14:36:12 -0500

    feat: creando migración para la creación de la tabla users en la base de datos

---
+++ src/common/migrations/1715276400000-CreateUsersTable.ts
@@ -0,0 +1,70 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsersTable1715276400000 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        // 1. Asegurar la extensión para UUIDs
+        await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+
+        // 2. Crear la tabla
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                        isUnique: true,
+                        generationStrategy: "uuid",
+                        default: "uuid_generate_v4()",
+                    },
+                    {
+                        name: "email",
+                        type: "varchar",
+                        isUnique: true,
+                        isNullable: false,
+                    },
+                    {
+                        name: "password",
+                        type: "varchar",
+                        isNullable: false,
+                    },
+                    {
+                        name: "first_name",
+                        type: "varchar",
+                        length: "100",
+                        isNullable: false,
+                    },
+                    {
+                        name: "last_name",
+                        type: "varchar",
+                        length: "100",
+                        isNullable: false,
+                    },
+                    {
+                        name: "isActive",
+                        type: "boolean",
+                        default: true,
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                ],
+            }),
+            true
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
+    }
+
+}
