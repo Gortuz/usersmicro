d16273ce12e594d0aaec7e733563b5b6263e23b3
Alan Sandoval <alansandovalproject@gmail.com>
2026-05-09 15:16:48 -0500

    feat: creando controller con tópicos de kafka

---
+++ src/users/infrastructure/controllers/users.controller.ts
@@ -0,0 +1,47 @@
+import { Controller } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { CreateUserCommand } from '../../application/commands/create-user/create-user.command';
+import { UpdateUserCommand } from '../../application/commands/update-user/update-user.command';
+import { DeleteUserCommand } from '../../application/commands/delete-user/delete-user.command';
+import { GetAllUsersQuery } from '../../application/queries/get-all-users/get-all-users.query';
+import { GetUserByIdQuery } from '../../application/queries/get-user-by-id/get-user-by-id.query';
+
+@Controller()
+export class UsersController {
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+  ) {}
+
+  @MessagePattern('user.create')
+  async create(@Payload() data: any) {
+    const { email, password, firstName, lastName } = data;
+    return await this.commandBus.execute(
+      new CreateUserCommand(email, password, firstName, lastName),
+    );
+  }
+
+  @MessagePattern('user.find.all')
+  async findAll() {
+    return await this.queryBus.execute(new GetAllUsersQuery());
+  }
+
+  @MessagePattern('user.find.one')
+  async findOne(@Payload() data: { id: string }) {
+    return await this.queryBus.execute(new GetUserByIdQuery(data.id));
+  }
+
+  @MessagePattern('user.update')
+  async update(@Payload() data: any) {
+    const { id, email, firstName, lastName, password } = data;
+    return await this.commandBus.execute(
+      new UpdateUserCommand(id, email, firstName, lastName, password),
+    );
+  }
+
+  @MessagePattern('user.delete')
+  async remove(@Payload() data: { id: string }) {
+    return await this.commandBus.execute(new DeleteUserCommand(data.id));
+  }
+}
